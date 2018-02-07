14deb5c8fb18359738d9e932d0f1a60ea5cfecdf
Darren Chiu <chiusw2003@gmail.com>
2018-02-07 20:55:37 +0800

    added prototype of the experiment logic

--- test-experiment.py
+++ test-experiment.py
@@ -1,4 +1,5 @@
 import expyriment
+import random
 
 exp = expyriment.design.Experiment(name="First Experiment")
 expyriment.control.initialize(exp)
@@ -6,17 +7,10 @@ expyriment.control.initialize(exp)
 block = expyriment.design.Block(name="A name for the block")
 trial = expyriment.design.Trial()
 
-stim = expyriment.stimuli.Picture("img-sources/icons8-android-os.png")
-stim.preload()
-trial.add_stimulus(stim)
-
-stim = expyriment.stimuli.Picture("img-sources/icons8-android-tablet.png")
-stim.preload()
-trial.add_stimulus(stim)
-
-stim = expyriment.stimuli.Picture("img-sources/icons8-app-windows.png")
-stim.preload()
-trial.add_stimulus(stim)
+for i in range(1, 101):
+    stim = expyriment.stimuli.TextLine(text=str(i))
+    stim.preload()
+    trial.add_stimulus(stim)
 
 block.add_trial(trial)
 exp.add_block(block)
@@ -27,10 +21,29 @@ expyriment.control.start()
 
 for block in exp.blocks:
     for trial in block.trials:
-        for stim in trial.stimuli:
-            stim.present()
+        current_number = random.randint(0, 100)
+        reverse_count = 0
+        last_action = 0
+        lower_bound = 0
+        upper_bound = 100
+
+        while reverse_count < 12:
+            trial.stimuli[current_number].present()
             key, rt = exp.keyboard.wait([expyriment.misc.constants.K_LEFT,
                                          expyriment.misc.constants.K_RIGHT])
             exp.data.add([block.name, trial.id, key, rt])
+
+            if key == expyriment.misc.constants.K_LEFT:
+                step = random.randint(0, upper_bound - current_number)
+                lower_bound = current_number
+                current_number = current_number + step
+            elif key == expyriment.misc.constants.K_RIGHT:
+                step = random.randint(0, current_number - lower_bound)
+                upper_bound = current_number
+                current_number = current_number - step
+
+            if last_action != key:
+                last_action = key
+                reverse_count += 1
 
 expyriment.control.end()
