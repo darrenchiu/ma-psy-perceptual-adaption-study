e7f46b3d161e3e35d19fd8d152acdb48036fa401
Darren Chiu <chiusw2003@gmail.com>
2018-03-05 00:30:31 +0800

    adding clearer instructions

--- experiment.py
+++ experiment.py
@@ -4,15 +4,19 @@ import random
 # Constants
 BLANK = 0
 RESPOND = 101
-INSTRUCTION = 102
+INSTRUCTION_FOR_DECISION = 102
+INSTRUCTION_FOR_PRIMING = 103
+INSTRUCTION_FOR_START_PRIMING = 104
+INSTRUCTION_FOR_START_EXPERIEMENT = 105
 # control condition
 CONDITION = 1
 STEP = 5
 # priming time should be 180000 (3 mins)
-PRIMING_TIME = 10
+PRIMING_TIME = 1000
 
 
 def PrepareTrial(emotion_pair, set, left_emotion, right_emotion):
+    trial = expyriment.design.Trial()
     blank_screen = expyriment.stimuli.BlankScreen()
     blank_screen.preload()
     trial.add_stimulus(blank_screen)
@@ -25,31 +29,55 @@ def PrepareTrial(emotion_pair, set, left_emotion, right_emotion):
     respond_screen.preload()
     trial.add_stimulus(respond_screen)
 
-    instruction_screen = expyriment.stimuli.TextLine(
-        'When prompted to response, press <Left> key for {0}, press <Right> key for {1}. When ready, press <Enter> key.'.format(
-            left_emotion, right_emotion))
+    instruction_screen = expyriment.stimuli.TextBox(
+        'When prompted to response, press <Left> key for {0}, press <Right> key for {1}.\n\n When ready, press <Enter> key.'.format(
+            left_emotion, right_emotion), (1000, 500))
     instruction_screen.preload()
     trial.add_stimulus(instruction_screen)
+
+    priming_instruction_screen = expyriment.stimuli.TextBox(
+        'Before the experiement, we will first show you a face for 3 minutes.\n\nFor each response, we will show you a face for 5 seconds before flashing you the face that you need to respond.\n\nWhen you are ready to start, press <Enter> Key.', (1000, 500))
+    priming_instruction_screen.preload()
+    trial.add_stimulus(priming_instruction_screen)
+
+    start_priming_screen = expyriment.stimuli.TextBox('Now we will show you a face for 3 minutes.\n\nPlease focus on it.\n\nWhen ready, press <Enter> key.', (1000, 500))
+    start_priming_screen.preload()
+    trial.add_stimulus(start_priming_screen)
+
+    start_experiment_screen = expyriment.stimuli.TextBox('Now experiment starts.\n\nWhen ready, press <Enter> key.', (1000, 500))
+    start_experiment_screen.preload()
+    trial.add_stimulus(start_experiment_screen)
+
     return trial
 
 
 def PerformExperimentTrial(block_idx, trial):
     # show instruction
-    trial.stimuli[INSTRUCTION].present()
+    trial.stimuli[INSTRUCTION_FOR_DECISION].present()
+    exp.keyboard.wait([expyriment.misc.constants.K_RETURN])
+
+    trial.stimuli[INSTRUCTION_FOR_PRIMING].present()
+    exp.keyboard.wait([expyriment.misc.constants.K_RETURN])
+
+    trial.stimuli[INSTRUCTION_FOR_START_PRIMING].present()
     exp.keyboard.wait([expyriment.misc.constants.K_RETURN])
 
     # adaption
     trial.stimuli[CONDITION].present()
     exp.clock.wait(PRIMING_TIME)
+
+    trial.stimuli[INSTRUCTION_FOR_START_EXPERIEMENT].present()
+    exp.keyboard.wait([expyriment.misc.constants.K_RETURN])
+
     current_number = 50
     reverse_count = 0
     last_action = 0
     while reverse_count < 12:
-        if reverse_count != 0:
-            trial.stimuli[CONDITION].present()
-            exp.clock.wait(5000)
-            trial.stimuli[0].present()
-            exp.clock.wait(250)
+
+        trial.stimuli[CONDITION].present()
+        exp.clock.wait(5000)
+        trial.stimuli[0].present()
+        exp.clock.wait(250)
 
         trial.stimuli[current_number].present()
         exp.clock.wait(500)
@@ -73,7 +101,7 @@ def PerformExperimentTrial(block_idx, trial):
 
 def PerformControlTrial(block_idx, trial):
     # show instruction
-    trial.stimuli[INSTRUCTION].present()
+    trial.stimuli[INSTRUCTION_FOR_DECISION].present()
     exp.keyboard.wait([expyriment.misc.constants.K_RETURN])
 
     current_number = 50
@@ -102,28 +130,27 @@ def PerformControlTrial(block_idx, trial):
 
 
 # Main Program
-exp = expyriment.design.Experiment(name="Experiment - Condition: " + str(CONDITION))
+exp = expyriment.design.Experiment(name="Visual Perception Experiement")
 expyriment.control.initialize(exp)
 
 block = expyriment.design.Block(name="A name for the block")
-trial = expyriment.design.Trial()
 
-PrepareTrial("HA-AN", 1, "HAPPY", "ANGRY")
-
-block.add_trial(trial)
+block.add_trial(PrepareTrial("HA-AN", 1, "HAPPY", "ANGRY"))
+block.add_trial(PrepareTrial("HA-AN", 2, "HAPPY", "ANGRY"))
+block.add_trial(PrepareTrial("DI-SU", 1, "DISGUST", "SURPRISED"))
+block.add_trial(PrepareTrial("DI-SU", 2, "DISGUST", "SURPRISED"))
 exp.add_block(block)
 
 exp.data_variable_names = ["Block", "Trial", "Key", "RT", "Current"]
 expyriment.control.set_develop_mode(True)
 expyriment.control.start()
 
-for block in exp.blocks:
-    for trial in block.trials:
-        PerformControlTrial(1, trial)
+# for block in exp.blocks:
+#     for trial in block.trials:
+#         PerformControlTrial(1, trial)
 
 for block in exp.blocks:
     for trial in block.trials:
         PerformExperimentTrial(2, trial)
-
 
 expyriment.control.end()
